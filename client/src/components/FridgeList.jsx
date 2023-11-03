56654abd760e2c2639bb597b2c4cea0e7eaf1dc4
simicity <simi40nm@gmail.com>
2023-11-03 00:42:57 -0700

    Add buttons to count up and down food items in the fridge list

--- client/src/components/FridgeList.jsx
+++ client/src/components/FridgeList.jsx
@@ -7,6 +7,12 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { useState, useCallback } from 'react';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -23,17 +29,28 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 function createData(name, category, addedDate, expirationDate, count) {
   return { name, category, addedDate, expirationDate, count };
 }
-
-const rows = [
-  createData('Frozen yoghurt', "Dairy", 6.0, 24, 4.0),
-  createData('Ice cream sandwich', "Dairy", 9.0, 37, 4.3),
-  createData('Eclair', "Sweets", 16.0, 24, 6.0),
-  createData('Cupcake', "Sweets", 3.7, 67, 4.3),
-  createData('Gingerbread', "Sweets", 16.0, 49, 3.9),
-];
 /* dummy data end */
 
 const FridgeList = () => {
+  const [items, setItems] = useState([
+    createData('Frozen yoghurt', "Dairy", 6.0, 24, 4.0),
+    createData('Ice cream sandwich', "Dairy", 9.0, 37, 4.3),
+    createData('Eclair', "Sweets", 16.0, 24, 6.0),
+    createData('Cupcake', "Sweets", 3.7, 67, 4.3),
+    createData('Gingerbread', "Sweets", 16.0, 49, 3.9),
+  ])
+
+  const handleCountUp = useCallback((index) => {
+    items[index].count += 1
+    setItems([...items])
+  }, [items]);
+
+  const handleCountDown = useCallback((index) => {
+    if(items[index].count <= 0) return
+    items[index].count -= 1
+    setItems([...items])
+  }, [items]);
+
   return (
     <TableContainer component={Paper}>
       <Table size="small">
@@ -48,16 +65,34 @@ const FridgeList = () => {
             </TableRow>
         </TableHead>
         <TableBody>
-          {(rows && rows.length > 0) ? rows.map((row) => (
+          {(items && items.length > 0) ? items.map((item, index) => (
             <TableRow
-              key={row.name}
+              key={item.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">{row.name}</TableCell>
-              <TableCell align="right">{row.category}</TableCell>
-              <TableCell align="right">{row.addedDate}</TableCell>
-              <TableCell align="right">{row.expirationDate}</TableCell>
-              <TableCell align="right">{row.count}</TableCell>
+              <TableCell component="th" scope="row">{item.name}</TableCell>
+              <TableCell align="right">{item.category}</TableCell>
+              <TableCell align="right">{item.addedDate}</TableCell>
+              <TableCell align="right">{item.expirationDate}</TableCell>
+              <TableCell align="right">
+                <Stack direction="row">
+                  <IconButton
+                    fontSize="small"
+                    onClick={() => handleCountUp(index)}
+                  >
+                    <ArrowDropUpIcon />
+                  </IconButton>
+                  <Box sx={{ flexGrow: 1, textAlign: "center", m: "auto" }}>
+                    {item.count}
+                  </Box>
+                  <IconButton
+                    fontSize="small"
+                    onClick={() => handleCountDown(index)}
+                  >
+                    <ArrowDropDownIcon />
+                  </IconButton>
+                </Stack> 
+              </TableCell>
               <TableCell align="right">
                 <Button variant="outlined" size="small">Add</Button>
               </TableCell>
