07386641ef869ac0b5717d0baa0acae4962dd322
Jiarui <lilyliang0021@gmail.com>
2023-11-03 01:37:40 -0500

    add fridges api

---
+++ server/routes/fridges.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import FridgesController from '../controllers/fridges.js'
+
+const router = express.Router()
+
+router.get('/', FridgesController.getFridges)
+router.get('/:id', FridgesController.getFridge)
+router.post('/:user_id', FridgesController.createFridge)
+router.patch('/:id', FridgesController.updateFridge)
+router.delete('/:id', FridgesController.deleteFridge)
+
+export default router
