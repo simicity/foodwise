d90627af1e03df361540f45a1f844ad754d4d14b
simicity <simi40nm@gmail.com>
2023-11-06 00:14:54 -0800

    Add landing page

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,41 +1,46 @@
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import MenuIcon from '@mui/icons-material/Menu';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import Toolbar from '@mui/material/Toolbar';
-import Header from '../components/Header.jsx';
-import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import Collapse from '@mui/material/Collapse'
+import MenuIcon from '@mui/icons-material/Menu'
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+import Toolbar from '@mui/material/Toolbar'
+import Header from '../components/Header.jsx'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { Outlet } from 'react-router-dom'
+import { API_URL } from '../main'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 const Root = (props) => {
-  const { window } = props;
-  const [fridges, setFridges] = useState([{id: 1, name:"Fridge 1"}, {id: 2, name:"Fridge 2"}]); // TODO: dummy data for UI testing; get fridge data from API
-  const [open, setOpen] = useState(true);
-  const [mobileOpen, setMobileOpen] = useState(false);
+  const { window } = props
+  const [fridges, setFridges] = useState([{id: 1, name:"Fridge 1"}, {id: 2, name:"Fridge 2"}]) // TODO: dummy data for UI testing get fridge data from API
+  const [open, setOpen] = useState(true)
+  const [mobileOpen, setMobileOpen] = useState(false)
+  const user = useSelector(state => state.user.user)
 
   const handleNestedListToggle = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    setMobileOpen(!mobileOpen)
+  }
 
   useEffect(() => {
     //TODO: set fridge data
-  }, []);
+  }, [])
 
   const drawer = (
     <div>
@@ -62,9 +67,9 @@ const Root = (props) => {
         </Collapse>
       </List>
     </div>
-  );
+  )
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container = window !== undefined ? () => window().document.body : undefined
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -94,47 +99,63 @@ const Root = (props) => {
       </AppBar>
 
       {/* Sidebar */}
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-      >
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
+      {user && user.id && (
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+          >
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      )}
 
       {/* Main */}
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
-        <Toolbar />
-        <Outlet />
-      </Box>
+      {user && user.id ? (
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+          <Toolbar />
+          <Outlet />
+        </Box>
+      ) : (
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3 }}
+        >
+          <Toolbar />
+          <Box sx={{ m: "auto", mt: 10, maxWidth: "600px", textAlign: "center" }}>
+            <Typography variant="h4" component="h1">Welcome to <b>Foodwise</b><br /> – Your ultimate food inventory manager!</Typography>
+            <Typography variant="subtitle1" component="p" mt={2}>Say goodbye to pantry chaos and hello to easy organization. Keep track of your groceries, reduce food waste, and whip up delicious meals with confidence.</Typography>
+            <Button variant='contained' component="a" href={`${API_URL}/auth/google`} size='large' color='primary' sx={{ mt: 6 }}>Get Started!</Button>
+          </Box>
+        </Box>
+      )}
     </Box>
-  );
+  )
 }
 
 Root.propTypes = {
@@ -143,6 +164,6 @@ Root.propTypes = {
    * You won't need it on your project.
    */
   window: PropTypes.func,
-};
+}
 
-export default Root;
+export default Root
