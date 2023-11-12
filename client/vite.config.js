b2db801de05710eee5586a4ede910e9100ec214b
simicity <simi40nm@gmail.com>
2023-11-11 19:42:05 -0800

    Add production setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001'
-      },
-      '/auth': {
-        target: 'http://localhost:3001'
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const isProduction = mode === 'production'
+
+    return {
+      ...defineConfig,
+      plugins: [react()],
+      server: {
+        proxy: {
+          '/api': {
+            target: isProduction ? 'foodwise-server.up.railway.app' : 'http://localhost:3001'
+          },
+          '/auth': {
+            target: isProduction ? 'foodwise-server.up.railway.app' : 'http://localhost:3001'
+          }
+        }
       }
+    }
+  } else {
+    return {
+      ...defineConfig,
+      plugins: [react()]
     }
   }
 })
